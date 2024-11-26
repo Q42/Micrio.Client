ac46fa9e1dc6b274033047a636a2b00b7c653d45
Marcel Duin <marcel@q42.nl>
2024-11-26 14:10:57 +0900

    HLS support feature detection, not browser fingerprinting

--- src/svelte/components/Media.svelte
+++ src/svelte/components/Media.svelte
@@ -237,8 +237,7 @@
 	if(isCFVid && src) {
 		// IOS can straight up play vid IDs
 		cfVidSrc = `https://videodelivery.net/${src.slice(8)}/manifest/video.m3u8`;
-		if(Browser.iOS) realSrc = cfVidSrc;
-		else realSrc = undefined;
+		realSrc = undefined;
 	}
 
 	// Micrio video tour
@@ -490,8 +489,9 @@
 	}
 
 	async function loadPlayer() : Promise<void> {
+		const hasNativeHLS = _media?.canPlayType('application/vnd.apple.mpegurl') || _media?.canPlayType('application/x-mpegURL');
 		// Print cloudflare vid using hls.min.js
-		if(isCFVid && !Browser.iOS) return loadScript('https://i.micr.io/hls-1.4.5.min.js', undefined, 'Hls' in window ? {} : undefined).then(() => {
+		if(isCFVid && !hasNativeHLS) return loadScript('https://i.micr.io/hls-1.4.5.min.js', undefined, 'Hls' in window ? {} : undefined).then(() => {
 			/** @ts-ignore */
 			hlsPlayer = new (window['Hls'] as HlsPlayer)();
 			hlsPlayer.loadSource(cfVidSrc);
@@ -501,7 +501,10 @@
 			if(frameType == FrameType.YouTube) return loadYouTube();
 			if(frameType == FrameType.Vimeo) return loadVimeo();
 		}
-		else if(_media) hookMedia();
+		else if(_media) {
+			if(isCFVid) realSrc = cfVidSrc;
+			hookMedia();
+		}
 	}
 
 	// Hook <video> or <audio> events
@@ -649,7 +652,7 @@
 			<video controls={false} loop={loop && (loopDelay == undefined || loopDelay <= 0)} playsinline width={rWidth} height={rHeight} bind:this={_media}
 				bind:duration bind:muted autoplay={!!autoplay} bind:currentTime bind:seeking bind:paused on:canplay={canplay} on:ended={ended}>{#if realSrc}
 				{#if hasTransparentH265 && realSrc.endsWith('.webm')}<source src={realSrc.replace('.webm', '.mp4')} type="video/mp4;codecs=hvc1">{/if}
-				<source src={realSrc}>{/if}
+				<source src={realSrc} type={isCFVid ? 'application/x-mpegURL' : undefined}>{/if}
 			</video>
 		{:else if type == Type.Audio && !singleAudio}
 			<audio src={realSrc} controls={false} bind:this={_media} bind:duration bind:muted
