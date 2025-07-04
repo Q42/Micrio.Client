3ef97b668b97d70b0fd20f969e61ff02d314b992
Marcel Duin <duin.marcel@gmail.com>
2025-07-04 12:17:24 +0900

    v5.3.6 -> v5.3.7

--- src/ts/version.ts
+++ src/ts/version.ts
@@ -2,4 +2,4 @@
  * Defines the current version of the Micrio library.
  * This constant is used internally and exposed statically via `HTMLMicrioElement.VERSION`.
  */
-export const VERSION = '5.3.6';
+export const VERSION = '5.3.7';
