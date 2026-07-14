7cfce3f74c025f81f2b6f26e4f3445000f06550e
Marcel Duin <duin.marcel@gmail.com>
2026-07-14 11:28:13 +0900

    don't place empty elements

--- src/components/micrio-main.ts
+++ src/components/micrio-main.ts
@@ -100,6 +100,10 @@ export class MicrioMain extends MicrioElement<MainProps> {
 			const el = build();
 			this.#elements.set(key, el);
 			this.#place(key, el);
+			if (!el.children.length) {
+				el.remove();
+				this.#elements.set(key, null);
+			}
 		} else if (existing?.isConnected) {
 			existing.remove();
 			this.#elements.set(key, null);
